fe05224e82c2fd556deb9a1290f9719ee65c5c8e
Harshdeep Singh <harshdeepsingh87179@gmail.com>
2026-07-09 14:11:02 +0530

    feat(logging): add structured application logging

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,14 +1,22 @@
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import pinoHttp from "pino-http";
 
 import healthRoutes from "./routes/health.routes.js";
+import { logger } from "./shared/logger/logger.js";
 
 const app = express();
 
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+
+app.use(
+  pinoHttp({
+    logger,
+  })
+);
 
 app.use("/api", healthRoutes);
 
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import app from "./app.js";
 import { env } from "./config/env.js";
+import { logger } from "./shared/logger/logger.js";
 
 app.listen(env.PORT, () => {
-  console.log(`GatePulse server running on port ${env.PORT}`);
+  logger.info(`GatePulse server running on port ${env.PORT}`);
 });
